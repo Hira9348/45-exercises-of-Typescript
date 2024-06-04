b37a2379a38034bad34ba6ff0f7482659a7a6172
Hira9348 <hiraimi608@gmail.com>
2024-06-04 06:35:58 -0400

    Add files via upload

---
+++ exercise 13/main.js
@@ -0,0 +1,2 @@
+var transportationMode = ["Jeep", "Car", "Heavy Bike",];
+transportationMode.forEach(function (mode) { return console.log("I would like to own a ".concat(mode)); });
---
+++ exercise 13/main.ts
@@ -0,0 +1,3 @@
+let transportationMode = ["Jeep","Car","Heavy Bike",]
+
+transportationMode.forEach(mode => console.log(`I would like to own a ${mode}`))
