cc8f8c846185d36db1c9d124e94313b6b5665afa
Hira9348 <hiraimi608@gmail.com>
2024-06-04 06:34:57 -0400

    Add files via upload

---
+++ excercise 12/main.js
@@ -0,0 +1,2 @@
+var friendsName = ["Hira", "Fatima", "saira"];
+friendsName.forEach(function (friendName) { return console.log("Hello, ".concat(friendName, " how are you?")); });
---
+++ excercise 12/main.ts
@@ -0,0 +1,3 @@
+let friendsName = ["Hira","Fatima","saira"];
+
+friendsName.forEach(friendName => console.log(`Hello, ${friendName} how are you?`))
