b0e771f3257054b2e525e58641bd5319a00364d9
Hira9348 <hiraimi608@gmail.com>
2024-06-04 06:36:52 -0400

    Add files via upload

---
+++ exercise 14/main.js
@@ -0,0 +1,2 @@
+var guestlist = ["Shazia", "Fouzia", "Rabia"];
+guestlist.forEach(function (member) { return console.log("Assalm o alikum ".concat(member, ", would you like to dinner with me?")); });
---
+++ exercise 14/main.ts
@@ -0,0 +1,3 @@
+let guestlist = ["Shazia","Fouzia","Rabia"]
+
+guestlist.forEach(member => console.log(`Assalm o alikum ${member}, would you like to dinner with me?`))
